4f383d99b559e60e522fe86da832ef23ac30b322
Alexander Toropov <torf993@gmail.com>
2014-07-27 17:53:46 +0400

    Правка батника

--- install.bat
+++ install.bat
@@ -1,12 +1,12 @@
 @echo off
 chcp 1251
 set CD2=%CD:\=\\%
-set verfile=protocol.reg
-set tmpfile=support.reg
+set verfile=source\\protocol.reg
+set tmpfile=source\\support.reg
 set seek=instPath
 if exist %tmpfile% del /q %tmpfile%
 for /f "delims=" %%a in (%verfile%) do (
-  (echo %%a)|>nul find /i "%seek%"&&((echo @="%CD2%\\RSP.exe")>>%tmpfile%)
+  (echo %%a)|>nul find /i "%seek%"&&((echo @="%CD2%\\source\\RSP\\RSP.exe")>>%tmpfile%)
   (echo %%a)|>nul find /i "%seek%"||(echo %%a)>>%tmpfile%
 )
 
